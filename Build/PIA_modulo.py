2725bd3a190f9bcb3268b67a32c16482ebfaa8e4
jimenatorresa <jimena.torresa@uanl.edu.mx>
2025-05-12 17:44:21 -0600

    Update PIA_modulo.py

--- Build/PIA_modulo.py
+++ Build/PIA_modulo.py
@@ -1,48 +1,26 @@
 import requests
 import json
 import statistics
-
-url="https://restcountries.com/v3.1/region/america?fields=name,area,unMember,languages,independent,population"
-
-respuesta = requests.get(url)
-lista_datos=[]
-
-if respuesta.status_code==200:
-	datos=respuesta.json()
-	with open('datos.json', 'w') as archivo:
-		json.dump(datos, archivo, indent=4)
-
-	for pais in datos:
-		if pais.get("independent")==True:
-			nombre=pais["name"]["common"]
-			area=pais["area"]
-			lenguajes=list(pais["languages"].values())
-			miembro_onu=pais["unMember"]
-			poblacion=pais["population"]
-
-			lista_datos.append({"País":nombre, "Área": area, "Lenguajes": lenguajes, "Población": poblacion, "Miembro de la Onu": miembro_onu,})
-	
-
-
-datos_america=str(lista_datos)
-
-with open('paises_america.txt', 'a') as archivo:
-	archivo.write(datos_america)
-
-
-def main_areas(lista_datos):
-	areas=[]
-	for i in range (len(lista_datos)):
-		a=lista_datos[i]["Área"]
-		areas.append(a)
-	return areas
-main_areas(lista_datos)
-
-def analisis_areas(areas):
-	print(f"El area promedio de los países en Ámerica es: {statistics.mean(areas):.2f} kilometros cuadrados")
-	print(f"Mediana: {statistics.median(areas):.2f} kilometros cuadrados")
-areas=main_areas(lista_datos)
-analisis_areas(areas)
+def obtener_datos():
+	url="https://restcountries.com/v3.1/region/america?fields=name,area,unMember,languages,independent,population,translations"
+	respuesta = requests.get(url)
+	lista_datos=[]
+	if respuesta.status_code==200:
+		datos_paises=respuesta.json()
+		with open('datos_paises.json', 'w') as archivo:
+			json.dump(datos_paises, archivo, indent=4)
+		for pais in datos_paises:
+			if pais.get("independent")==True:
+				nombre=pais["translations"]["spa"]["common"]
+				area=pais["area"]
+				lenguajes=list(pais["languages"].values())
+				miembro_onu=pais["unMember"]
+				poblacion=pais["population"]
+				lista_datos.append({"País":nombre, "Área": area, "Lenguajes": lenguajes, "Población": poblacion, "Miembro de la Onu": miembro_onu,})
+		datos_america=str(lista_datos)
+		with open('paises_america.txt', 'w') as archivo:
+			archivo.write(datos_america)
+		return lista_datos 
 
 
 
